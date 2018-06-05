4e111df020315e9ebfe4feadf741adaa10d16452
Kyungsoo Lee <dlrudtn1008@gmail.com>
2018-06-05 12:17:24 +0900

    BasicScorerTest_1st_이경수
    첫번째 시도입니다.

--- Assets/Models/Scorers/BasicScorer.cs
+++ Assets/Models/Scorers/BasicScorer.cs
@@ -15,10 +15,15 @@
 
         private int CalculateScore(Player player)
         {
+            //player의 첫번째 카드와 두번째 카드가 광일 경우에는 101의 점수를 반환한다.
             if (player[0].IsKwang && player[1].IsKwang)
                 return 101;
+
+            //player의 첫번째 카드와 두번째 카드가 같은 숫자일 경우에 숫자에 10을 곱한다.
             else if (player[0].No == player[1].No)
                 return player[0].No * 10;
+
+            //player의 첫번째 카드와 두번째 카드가 광이 아닐 경우에는 두 숫자를 더한 후에 10을 나눈다.
             else
                 return (player[0].No + player[1].No) % 10;
         }
--- Assets/Tests/BasicScorerTest_이경수.cs
+++ Assets/Tests/BasicScorerTest_이경수.cs
@@ -1,12 +1,66 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityDemo.Models;
+using UnityDemo.Models.Scorers;
 
 namespace Assets.Tests
 {
     class BasicScorerTest_이경수
     {
+
+        [Test]
+        public void 광땡으로_이기는_경우()
+        {
+            Scorer scorer = new BasicScorer();
+            Player p1 = new Player("문슬기범");
+            p1.AddCard(new Card(8, false));
+            p1.AddCard(new Card(1, false));
+
+            Player p2 = new Player("박혜강");
+            p2.AddCard(new Card(3, true));
+            p2.AddCard(new Card(1, true));
+
+            Player winner = scorer.GetWinner(p1, p2);
+
+            Assert.AreEqual(p2, winner);
+        }
+
+        [Test]
+        public void 땡으로_이기는_경우()
+        {
+            Scorer scorer = new BasicScorer();
+            Player p1 = new Player("문슬기범");
+            p1.AddCard(new Card(8, false));
+            p1.AddCard(new Card(8, false));
+
+            Player p2 = new Player("박혜강");
+            p2.AddCard(new Card(7, false));
+            p2.AddCard(new Card(2, false));
+
+            Player winner = scorer.GetWinner(p1, p2);
+
+            Assert.AreEqual(p1, winner);
+        }
+
+        [Test]
+        public void 끗으로_이기는_경우()
+        {
+            Scorer scorer = new BasicScorer();
+            Player p1 = new Player("문슬기범");
+            p1.AddCard(new Card(2, false));
+            p1.AddCard(new Card(5, false));
+
+            Player p2 = new Player("박혜강");
+            p2.AddCard(new Card(4, false));
+            p2.AddCard(new Card(6, false));
+
+            Player winner = scorer.GetWinner(p1, p2);
+
+            Assert.AreEqual(p1, winner);
+        }
     }
 }
